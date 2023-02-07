6a282105badc18e307de6a8be5284998298aabdb
Daniel Thalmann <daniel@thalmann.li>
2023-02-07 23:32:40 +0100

    script for duplicate object

---
+++ Assets/Scripts/Duplicate.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicate : MonoBehaviour
+{
+    public GameObject duplicate;
+    public int amount = 2;
+    public float speed = 1.0f;
+    public float minimalDistance = 1.0f;
+
+    private List<GameObject> chains;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = transform.position;
+        chains = new List<GameObject>();
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject newObject = Instantiate(duplicate, this.transform);
+            chains.Add(newObject);
+        }
+
+        Debug.Log(chains.Count);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist;
+        for (int i = 1; i < amount; i++)
+        {
+            dist = Vector3.Distance(chains[i].transform.position, chains[i - 1].transform.position);
+
+            if (dist > minimalDistance)
+                chains[i].transform.position = Vector3.Lerp(chains[i].transform.position, chains[i - 1].transform.position, speed * Time.deltaTime);
+            /*
+            else if(dist < minimalDistance - 0.2f)
+            {
+                Vector3 diff = chains[i].transform.position - chains[i - 1].transform.position;
+                chains[i].transform.position = Vector3.Lerp(chains[i].transform.position, diff, speed * Time.deltaTime);
+            }
+            */
+        }
+    }
+}
